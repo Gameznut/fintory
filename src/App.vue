d80d7b14278acd78dd5afe78a2041b3dc48b23c7
emmanuel apabiekun <31988283+Gameznut@users.noreply.github.com>
2022-05-22 23:09:18 +0100

    feomzie

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Navigation from "./components/Navigation.vue";
+import CardList from "./components/CardList.vue";
+import Software from "./components/Software.vue";
+</script>
+
+<template>
+  <header class="bg-green-400 max-w-[350px] w-full">
+    <Navigation />
+  </header>
+
+  <main class="w-full flex">
+    <section class="bg-[#F8F8F8 ] max-w-[492px] h-[100vh] w-full py-[56px]">
+      <CardList />
+    </section>
+    <section class="px-[86px] pt-[56px] w-full g-red-400">
+      <Software/>
+    </section>
+  </main>
+</template>
+
+<style>
+#app {
+  display: flex;
+}
+</style>
